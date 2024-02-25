e9b5f34b9f7d2752609a9f442e45cffbb851a453
Michal Pavlíček <michaelg.pavlicek@gmail.com>
2024-02-25 20:51:06 +0100

    Files updated on 2024-02-25 20:51:06

--- git.sh
+++ git.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+echo Pulling from remote
 git pull
+
+echo Adding to git, commiting and pushing to GitHub
 git add .;
 git commit -m "Files updated on `date +'%Y-%m-%d %H:%M:%S'`";
 git push origin main
